366206ed0bed198b5982ced05964d60a3179e7b5
armandmt <amata3@xtec.cat>
2021-02-04 22:17:04 +0100

    ddddd

---
+++ vue/vue2.js
@@ -0,0 +1,15 @@
+const app= Vue.createApp({
+
+ data() {
+     return {
+         titol : "El meu banc amb Vue.js",
+         quantitat: 1000,
+         enllac: 'https://youtube.com',
+
+     }
+ }
+
+
+})
+
+    
