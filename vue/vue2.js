d3ec58ace11255668a8316688dca662f1255bea4
armandmt <amata3@xtec.cat>
2021-02-05 14:04:35 +0100

    aaaa

--- vue/vue2.js
+++ vue/vue2.js
@@ -3,13 +3,31 @@ const app= Vue.createApp({
  data() {
      return {
          titol : "El meu banc amb Vue.js",
-         quantitat: 100,
+         quantitat: 1000,
          enllac: 'https://youtube.com',
          estat: false,
+         serveis:  [ "Transferències","Pagaments","Girs", "Xecs"],
+         desactivar: false,
 
      }
- }
+ },
 
+ methods: {
+    afegirSaldo()  {
+        this.quantitat = this.quantitat + 100;
+
+        if (this.quantitat > 0) {
+            this.desactivar= false
+        }
+    },
+    reduirSaldo()  {
+        
+        this.quantitat = this.quantitat - 100;
+        if (this.quantitat <= 0) {
+            this.desactivar= true            
+        }
+    }
+ }
 
 })
 
