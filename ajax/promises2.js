1df60d8be078cacde6b8570adb5eba0c2f562931
armandmt <amata3@xtec.cat>
2021-02-24 01:38:17 +0100

    asaa

--- ajax/promises2.js
+++ ajax/promises2.js
@@ -3,22 +3,46 @@ var valors = [10,18,6,20,30,40];
 
 recull  = ( id ) => {
 
-
     return new Promise (( resolve, reject) => {
 
-
-            if ( id in valors) {
-
+            if ( valors.includes(id)) {
+                console.log("resolve");
                 resolve(id);
             }
             else{
+                console.log("reject");
                 reject (id);
             }
 
-
     });
-
 
 }
 
-recull (10).then ( ide => console.log(ide));
+
+recullAsync  = async ( id ) => {
+
+    if ( valors.includes(id)) {
+         return id;  // resolve
+    }
+    else {
+        throw ("No existeix l'element " + id);  // reject
+    }
+};
+
+recullAsync (10)
+    .then ( ide => console.log(ide))
+    .catch( console.warn);
+recullAsync (12)
+    .then ( console.log)
+    .catch( ide=>console.warn(ide));
+ 
+
+/*recull (10)
+    .then ( ide => console.log(ide))
+    .catch( ide=>console.log(ide));
+
+recull (12)
+   .then ( ide => console.log(ide))
+   .catch( ide=>console.log(ide));
+
+*/
