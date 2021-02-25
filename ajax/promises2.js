e578971fef4ded6fef6c9d6702831b80cbe9d66e
Armand <amata3@xtec.cat>
2021-02-25 12:49:09 +0100

    aaa

--- ajax/promises2.js
+++ ajax/promises2.js
@@ -2,9 +2,7 @@
 var valors = [10,18,6,20,30,40];
 
 recull  = ( id ) => {
-
     return new Promise (( resolve, reject) => {
-
             if ( valors.includes(id)) {
                 console.log("resolve");
                 resolve(id);
@@ -13,14 +11,20 @@ recull  = ( id ) => {
                 console.log("reject");
                 reject (id);
             }
-
     });
-
 }
 
+recull (10)
+    .then ( ide => console.log(ide))
+    .catch( ide=>console.log(ide));
+recull (12)
+   .then ( ide => console.log(ide))
+   .catch( ide=>console.log(ide));
 
+
+ 
 recullAsync  = async ( id ) => {
-
+ 
     if ( valors.includes(id)) {
          return id;  // resolve
     }
@@ -35,14 +39,5 @@ recullAsync (10)
 recullAsync (12)
     .then ( console.log)
     .catch( ide=>console.warn(ide));
- 
 
-/*recull (10)
-    .then ( ide => console.log(ide))
-    .catch( ide=>console.log(ide));
 
-recull (12)
-   .then ( ide => console.log(ide))
-   .catch( ide=>console.log(ide));
-
-*/
