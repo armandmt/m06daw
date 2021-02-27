182747c79f9eb51ee4891fb7e12a6f342db7493a
Armand Mata <amata3@xtec.cat>
2021-02-27 02:16:27 +0100

    ZZZZZ

--- ajax/firebase1.js
+++ ajax/firebase1.js
@@ -1,4 +1,5 @@
 
+
 document.querySelector("#tasques").addEventListener("submit", function (e)
     {
         e.preventDefault();
@@ -10,9 +11,58 @@ document.querySelector("#tasques").addEventListener("submit", function (e)
 
         console.log ( {titol,descripcio,data})
 
-        // Enviar al servidor BD
-        // https://practiques-758e3-default-rtdb.firebaseio.com/
+        setDades ({titol,descripcio,data})
         
 
+        // Enviar al servidor BD
+        // https://practiques-758e3-default-rtdb.firebaseio.com/
+       
+
     }
-)
+    )    
+async function setDades(tasca) {   // Enviem un registre
+        try {
+            const res = await fetch(`https://practiques-758e3-default-rtdb.firebaseio.com/tasques.json`, {
+                method: 'POST',  // Genera un id aleatori
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(tasca)
+            })
+            const db = await res.json()
+            longi++
+            console.log(db)
+            
+        } catch (error) {
+            console.log(error)
+        }
+}
+
+
+async function visualitzaTasques() {
+
+
+        const res = await fetch("https://practiques-758e3-default-rtdb.firebaseio.com/tasques.json")
+        const db = await res.json()
+
+        let tabli = document.querySelector("#tasques")
+
+        for (i in db)
+        {
+            console.log(i);
+
+            let tr = document.createElement("tr")
+            tr.innerHTML = "<td>"+db[i].titol + "</td><td>"+db[i].descripcio+"</td>"
+            tabli.appendChild(tr)
+
+                     
+   
+            //db.descripcio
+            //db.data
+
+        }
+
+
+}
+    
+visualitzaTasques();
