af2de8bb6cb5483e7714b666534e3dea27c307fb
Armand <amata3@xtec.cat>
2021-04-09 16:30:39 +0200

    zzzz

---
+++ src/components/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div align="center">
+    <h1> Eppaaa ... aquí no has d'estar  </h1>
+    <img src="../assets/giphy.gif">
+  </div>
+</template>
+  
+<script>
+export default {
+
+    name: "NotFound",
+    setup () {
+
+
+        return {
+
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
