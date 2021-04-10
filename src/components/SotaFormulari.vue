74478d259b1b9e910815248d7bc0e771d7807941
armandmt <armandmt@gmail.com>
2021-04-10 21:08:11 +0200

    22

--- src/components/SotaFormulari.vue
+++ src/components/SotaFormulari.vue
@@ -1,11 +1,8 @@
 <template>
   <h1>Us de l'store per obtenir valors desats en un altre component</h1>
-  Sense ref: <b>{{ nom_del_store_sense_ref }}</b
-  ><br />
-  Amb ref <b>{{ nom_del_store_amb_ref }}</b
-  ><br />
-  computed <b>{{ nom_del_store_computed }}</b
-  ><br />
+  Sense ref: <b>{{ nom_del_store_sense_ref }}</b><br />
+  Amb ref <b>{{ nom_del_store_amb_ref }}</b><br />
+  computed <b>{{ nom_del_store_computed }}</b><br />
 </template>
 
 <script>
@@ -16,9 +13,9 @@ export default {
   name: "SotaFormulari",
   setup() {
     let store = useStore();
-    let nom_del_store_sense_ref = store.state.nom;
-    let nom_del_store_amb_ref = ref(store.state.nom);
-    let nom_del_store_computed = computed(() => store.state.nom);
+    let nom_del_store_sense_ref = store.state.nom;  // NO
+    let nom_del_store_amb_ref = ref(store.state.nom); // NO
+    let nom_del_store_computed = computed(() => store.state.nom);  // SI
 
     return {
       nom_del_store_sense_ref,
