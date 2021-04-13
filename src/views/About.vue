4d98274ee16c10a33b78696c0a535f6cb6a51e23
Armand <amata3@xtec.cat>
2021-04-13 11:05:31 +0200

    layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>Estem a l'about</h1>
-  </div>
+  <Layout>
+
+    <h1> estem a l'about </h1>
+    <h2> ia ia o </h2>
+    <router-link :to="{ name: 'Home' }">Tornem a caseta </router-link>
+  </Layout>
 </template>
+
+<script>
+import Layout from "../layouts/Layout"
+export default {
+name: "About",
+components: {
+    Layout,
+}
+
+}
+</script>
+
+<style>
+
+</style>
