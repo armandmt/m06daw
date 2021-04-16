085be94a561a8638a15e963cf766c7c354e387f7
Armand Mata <amata3@xtec.cat>
2021-04-16 16:40:01 +0200

    4567

---
+++ src/views/Game.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <h1> Epp, estem al Joc !!!!  </h1>
+  <h2> {{ $route.params.nom }} </h2>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+export default {
+
+
+    setup() {
+
+        // Aquí això dona error
+        // console.log($route.params.nom);
+
+        const ruta = useRoute();
+
+        console.log (ruta.params.nom);
+
+
+    }
+    
+
+}
+</script>
+
+<style>
+
+</style>
